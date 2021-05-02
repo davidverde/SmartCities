db9cea6e2e27169d2de2d3fd4047355f04669606
davidverde <61249154+davidverde@users.noreply.github.com>
2021-05-02 16:29:43 +0100

    Criação do layout adicionar anomalia

---
+++ app/src/main/java/com/example/smartcities/add_anom.kt
@@ -0,0 +1,15 @@
+package com.example.smartcities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class add_anom : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_anom)
+    }
+
+    fun addAnom_add(view: View) {}
+    fun voltarMapa_add(view: View) {}
+}
